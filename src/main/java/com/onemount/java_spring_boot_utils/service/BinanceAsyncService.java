c64d25aaa9004b7824ef0f098c01c86f83eea0ba
ntdat104 <ntd.k108cva@gmail.com>
2025-07-19 18:29:52 +0700

    update

--- src/main/java/com/onemount/java_spring_boot_utils/service/BinanceAsyncService.java
+++ src/main/java/com/onemount/java_spring_boot_utils/service/BinanceAsyncService.java
@@ -1,6 +1,7 @@
 package com.onemount.java_spring_boot_utils.service;
 
 import com.github.benmanes.caffeine.cache.Cache;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Async;
@@ -9,12 +10,18 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
 @Service
 public class BinanceAsyncService {
     private final RestTemplate restTemplate;
     private final String BASE_URL = "https://api.binance.com";
     private final String FUTURES_URL = "https://fapi.binance.com";
+
+    private final Integer delayTime = 400; //ms
+    private final Map<String, Long> cacheDelay = new HashMap<>();
 
     @Autowired
     @Qualifier("caffeineCache")
@@ -27,6 +34,12 @@ public class BinanceAsyncService {
     @Async
     public void fetchFromBinance(String symbol) {
         String key = "ticker:price:" + symbol;
+        Long prevTime = cacheDelay.get(key);
+        Long currentTime = System.currentTimeMillis();
+        if (prevTime != null && (currentTime < prevTime)) {
+            return;
+        }
+        cacheDelay.put(key, currentTime + delayTime);
         URI uri = UriComponentsBuilder.fromHttpUrl(BASE_URL + "/api/v3/ticker/price")
                 .queryParam("symbol", symbol)
                 .build().toUri();
